83c100e3f135e440d313798de5e92f527a9e4da1
Johannes Düsing <j.duesing13@googlemail.com>
2025-12-12 11:49:12 +0100

    Refractor new iterators to extract common functionality

--- src/main/java/org/tudo/sse/analyses/MavenCentralArtifactIterator.java
+++ src/main/java/org/tudo/sse/analyses/MavenCentralArtifactIterator.java
@@ -1,12 +1,9 @@
 package org.tudo.sse.analyses;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.tudo.sse.analyses.config.ArtifactAnalysisConfig;
 import org.tudo.sse.model.Artifact;
 import org.tudo.sse.model.ArtifactResolutionContext;
 import org.tudo.sse.model.index.IndexInformation;
-import org.tudo.sse.resolution.ResolverFactory;
 import org.tudo.sse.analyses.input.FileBasedArtifactIterator;
 import org.tudo.sse.utils.IndexIterator;
 import org.tudo.sse.utils.MavenCentralRepository;
@@ -20,22 +17,7 @@ import java.util.Iterator;
  *
  * @author Johannes Düsing
  */
-public class MavenCentralArtifactIterator implements Iterator<Artifact> {
-
-    private final Logger log = LoggerFactory.getLogger(MavenCentralArtifactIterator.class);
-    private final ArtifactAnalysisConfig config;
-
-    private Iterator<ArtifactResolutionContext> source;
-
-    private boolean badSource = false;
-
-    private long currentPosition = 0L;
-    private long artifactsTaken = 0L;
-    private long lastPositionSaved = 0L;
-
-    private final ResolverFactory resolverFactory;
-    private final boolean resolvePom;
-    private final boolean resolveJar;
+public final class MavenCentralArtifactIterator extends AbstractEntityIterator<ArtifactResolutionContext, Artifact> {
 
     /**
      * Creates a new iterator instance with the given configuration values.
@@ -46,120 +28,45 @@ public class MavenCentralArtifactIterator implements Iterator<Artifact> {
      * @param config The analysis configuration, see {@link org.tudo.sse.analyses.config.ArtifactAnalysisConfigBuilder}
      */
     public MavenCentralArtifactIterator(boolean resolvePom, boolean resolveTransitivePoms, boolean resolveJar, ArtifactAnalysisConfig config) {
-        this.config = config;
-        this.resolvePom = resolvePom;
-        this.resolveJar = resolveJar;
+        super(resolvePom, resolveTransitivePoms, resolveJar, config);
 
-        if(this.config.multipleThreads){
+        if(this.baseConfig.multipleThreads){
             log.warn("Artifact iterator does no support multiple threads - using a single thread for resolution");
         }
-
-        this.setUnderlyingSource();
-
-        if(this.config.outputEnabled){
-            this.resolverFactory = new ResolverFactory(true, this.config.outputDirectory, resolveTransitivePoms);
-        } else {
-            this.resolverFactory = new ResolverFactory(resolveTransitivePoms);
-        }
-
-        try {
-            if(!this.badSource)
-                this.skipInitial();
-        } catch(Exception x){
-            log.error("Failed to initialize iterator", x);
-            this.badSource = true;
-        }
     }
 
     @Override
-    public boolean hasNext() {
-        // If the underlying source is invalid, we have no next element
-        if(badSource) return false;
-
-        // If the config specifies a limited amount of artifacts to take, and that amount is reached, we have no next element
-        if(this.config.hasTake() && this.artifactsTaken >= this.config.take)
-            return false;
-
-        // Otherwise, we have a next artifact if the underlying source has a next element
-        try {
-            return this.source.hasNext();
-        } catch(Exception x){
-            // If accessing the source fails, we report an error and mark the source as bad - also we assume we have no
-            // next element.
-            log.error("Failed to access source", x);
-            this.badSource = true;
-            return false;
-        }
-
-    }
-
-    @Override
-    public Artifact next(){
-        if(!hasNext())
-            throw new IllegalStateException("No more artifacts available");
-
-        final ArtifactResolutionContext ctx = this.source.next();
+    protected Artifact buildEntity(ArtifactResolutionContext ctx){
         final Artifact artifact = ctx.getRootArtifact();
 
-        if(this.resolvePom){
-            resolverFactory.runPom(artifact.getIdent(), ctx);
-        }
-
-        if(this.resolveJar){
-            resolverFactory.runJar(artifact.getIdent(), ctx);
-        }
-
-        this.artifactsTaken += 1L;
-        this.currentPosition += 1L;
-
-        writePositionIfNeeded();
+        this.enrichArtifact(artifact, ctx);
 
         return artifact;
     }
 
-    private void writePositionIfNeeded(){
-        if(this.currentPosition - this.lastPositionSaved > this.config.progressWriteInterval){
-            AnalysisUtils.writePosition(this.currentPosition, this.config);
-            this.lastPositionSaved = this.currentPosition;
-        }
-    }
 
-    private void skipInitial(){
-        final long entitiesToSkip = AnalysisUtils.getInitialPosition(this.config);
-
-        if(entitiesToSkip > 0L && this.badSource)
-            return;
-
-        for(int i = 0; i < entitiesToSkip && this.source.hasNext(); i++){
-            this.source.next();
-            this.currentPosition += 1L;
-        }
-
-        log.debug("Successfully skipped to position {}", this.currentPosition);
-
-        if(!this.source.hasNext()){
-            log.warn("Reached end of input source while skipping to position {}", entitiesToSkip);
-        }
-    }
-
-
-    private void setUnderlyingSource(){
-        if(this.config.hasInputList()){
+    @Override
+    protected Iterator<ArtifactResolutionContext> buildSource(){
+        if(this.baseConfig.hasInputList()){
             try{
-                this.source = new MavenCentralCustomListArtifactSource();
+                return new MavenCentralCustomListArtifactSource();
             } catch(IOException | IllegalArgumentException x){
                 log.error("The given input list file is invalid", x);
                 this.badSource = true;
             }
         } else {
             try {
-                this.source = new MavenCentralIndexArtifactSource();
+                return new MavenCentralIndexArtifactSource();
             } catch(IOException uix){
                 log.error("Failed to access Maven Central Index", uix);
                 this.badSource = true;
             }
-
         }
+        return null;
+    }
+
+    private ArtifactAnalysisConfig getConfig(){
+        return (ArtifactAnalysisConfig) this.baseConfig;
     }
 
     private class MavenCentralIndexArtifactSource implements Iterator<ArtifactResolutionContext> {
@@ -187,10 +94,10 @@ public class MavenCentralArtifactIterator implements Iterator<Artifact> {
         }
 
         private boolean isValidInfo(IndexInformation indexInfo){
-            if(!config.hasTimeBasedFiltering()) return true;
+            if(!getConfig().hasTimeBasedFiltering()) return true;
             else {
                 long timeStamp = indexInfo.getLastModified();
-                return config.since <= timeStamp && timeStamp <= config.until;
+                return getConfig().since <= timeStamp && timeStamp <= getConfig().until;
             }
         }
 
@@ -229,7 +136,7 @@ public class MavenCentralArtifactIterator implements Iterator<Artifact> {
         private final FileBasedArtifactIterator list;
 
         MavenCentralCustomListArtifactSource() throws IOException {
-            list = new FileBasedArtifactIterator(config.inputListFile);
+            list = new FileBasedArtifactIterator(baseConfig.inputListFile);
             list.validateInput();
         }
 
